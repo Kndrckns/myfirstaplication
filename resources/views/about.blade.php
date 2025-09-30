425508fa42271d0db6cf25e6c32c038c5f7b28b7
Kendrick <keooanes@gmail.com>
2025-10-01 00:46:57 +0800

    Chapter 2: Implement dynamic job listings with Models

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-<x-slot:heading>
-About Page
-</x-slot:heading>
-This is the content for the about Page.
-</x-layout>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -81,9 +81,9 @@
     <div class="nav-links">
     <div class="hidden md:block">
 <div class="ml-10 flex items-baseline space-x-4">
+<!-- inside layout.blade.php's nav section -->
 <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
-<x-nav-link href="/about" :active="request()->is('about')">About</x-nav-link>
-<x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
+<x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
 </div>
 </div>
     </div>
